67b138bc83bc5caa7f8337287a3349fbc86705f5
klim <klim@hotmail.co.jp>
2017-02-13 18:16:43 +0900

    edit version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='regexXML',
-    version='0.1dev',
+    version='0.2dev',
     packages=['regexXML',],
     license='Apache 2.0',
     long_description=open('README.md').read(),
