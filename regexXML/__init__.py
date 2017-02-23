d8723a2b5a5c089d5d06ed6d10d29ac1848864f4
klim <klim@hotmail.co.jp>
2017-02-23 20:28:00 +0900

    faster regex & iterative parsing

--- regexXML/__init__.py
+++ regexXML/__init__.py
@@ -1,7 +1,7 @@
 import re
 from collections import OrderedDict
 
-attr_regex = re.compile(r"([\w\:]+?)\s*=\s*(?:\"([^\"'<>\/]+?)\"|\'([^<>\/']+?)\')")
+attr_regex = re.compile(r"([\w\:]+?)\s*=\s*(\"[^\"\'<>]+?\"|\'[^<>\']+?\')")
 
 def parse_attr(attr_str):
     attr = OrderedDict()
@@ -9,21 +9,61 @@ def parse_attr(attr_str):
         return attr
 
     for hit in attr_regex.finditer(attr_str):
-        if hit.group(2):
-            attr[hit.group(1)] = hit.group(2)
-        elif hit.group(3):
-            attr[hit.group(1)] = hit.group(3)
+        value = hit.group(2)
+        if (value[0] == "\"" and value[-1] == "\"") or (value[0] == "\'" and value[-1] == "\'"):
+            attr[hit.group(1)] = value[1:-1]
+        else:
+            attr[hit.group(1)] = value
     return attr
 
-def compile_tag_regex(tag):
-    '''
-    Return a regular expression object for parsing XML by a given tag name.
-    If elements with a same tag name exist on different levels,
-    innermost elements will be parsed.
-    '''
+class Tag:
 
-    return re.compile(
-        r"<%s(?:\s*|\s+(?P<attr>[^><\/]*?))(?:>(?P<inner>(?:(?!<%s[>\s]).)*?)</%s>|\/>)" % (tag, tag, tag),
-        re.DOTALL
-    )
+    def __init__(self, tag):
+        self.tag = tag
+        self.tag_regex = self.compile_tag_regex(tag)   
+
+    @classmethod
+    def compile_tag_regex(cls, tag):
+        '''
+        Return a regular expression object for parsing XML by a given tag name.
+        If elements with a same tag name exist on different levels,
+        innermost elements will be parsed.
+        '''
+        
+        return re.compile(
+            r"<%s(?: *| +(?P<attr>[^><]*?))(?:>(?P<inner>(?!<%s )(?!<%s>).*?)</%s>|\/>)" % (tag, tag, tag, tag),
+            re.DOTALL
+        )
+
+    def finditer(self, fileobj, batch_len=5000):
+        xml = ""
+
+        i = 0
+        for line in fileobj:
+            i += 1
+            xml += line
+            if i % batch_len:
+                continue
+
+            matches = [m for m in self.finditer_str(xml)]
+            if matches:
+                xml = xml[matches[-1].end():]  
+                for m in matches:
+                    yield m
+        
+        for m in self.finditer_str(xml):
+            yield m
+
+
+    def finditer_str(self, string):
+        for g in self.tag_regex.finditer(string):
+            yield g
+    
+    def search(self, iterable):
+        return self.finditer(iterable)
+
+    def search_str(self, string):
+        return self.tag_regex.search(string)
+
+
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='regexXML',
-    version='0.2dev',
+    version='0.3dev',
     packages=['regexXML',],
     license='Apache 2.0',
     long_description=open('README.md').read(),
