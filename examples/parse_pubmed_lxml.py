0992bd0ee928a654981878d7ec28d16cea7e4ac6
klim <klim@hotmail.co.jp>
2017-02-23 20:38:04 +0900

    add examples

---
+++ examples/parse_pubmed_lxml.py
@@ -0,0 +1,12 @@
+from lxml import etree
+
+root = etree.fromstring(open("pubmed1.xml").read())
+i = 0
+for AuthorList in root.findall(".//AuthorList"):
+    print("New aurhor list")
+    for Author in AuthorList.findall(".//Author"):
+        LastName = Author.find(".//LastName")
+        if LastName.text:
+            print("%s: %s" % (LastName.text, Author.get("ValidYN")))
+            i += 1
+print(i)
