dc278edb032c25ae29e2ad27d6de54069c273303
klim <klim@hotmail.co.jp>
2017-02-24 18:10:17 +0900

    edit examples

--- examples/parse_pubmed.py
+++ examples/parse_pubmed.py
@@ -1,15 +1,15 @@
-from regexXML import parse_attr, Tag
+from regexXML import Attr, Tag
 
 AuthorList_re = Tag("AuthorList")
 LastName_re = Tag("LastName")
 Author_re = Tag("Author")
 
 i = 0
-for AuthorList in AuthorList_re.finditer_str(open("pubmed1.xml").read()):
+for AuthorList in AuthorList_re.finditer(open("pubmed1.xml").read()):
     print("New author list")
-    for Author in Author_re.finditer_str(AuthorList.group()):
-        LastName = LastName_re.search_str(Author.group("inner"))
+    for Author in Author_re.finditer(AuthorList.group()):
+        LastName = LastName_re.search(Author.group("inner"))
         if LastName:
-            print("%s: %s" % (LastName.group("inner"), parse_attr(Author.group("attr")).get("ValidYN")))
+            print("%s: %s" % (LastName.group("inner"), Attr(Author.group("attr")).get("ValidYN")))
             i += 1
 print(i)
--- examples/parse_pubmed_lxml.py
+++ examples/parse_pubmed_lxml.py
@@ -3,7 +3,7 @@ from lxml import etree
 root = etree.fromstring(open("pubmed1.xml").read())
 i = 0
 for AuthorList in root.findall(".//AuthorList"):
-    print("New aurhor list")
+    print("New author list")
     for Author in AuthorList.findall(".//Author"):
         LastName = Author.find(".//LastName")
         if LastName.text:
