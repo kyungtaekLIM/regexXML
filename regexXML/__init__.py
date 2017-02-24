b4da93839fde4808dda77031218d3f844e4d4e77
klim <klim@hotmail.co.jp>
2017-02-24 18:09:14 +0900

    faster finditer

--- regexXML/__init__.py
+++ regexXML/__init__.py
@@ -1,20 +1,19 @@
 import re
 from collections import OrderedDict
 
-attr_regex = re.compile(r"([\w\:]+?)\s*=\s*(\"[^\"\'<>]+?\"|\'[^<>\']+?\')")
 
-def parse_attr(attr_str):
-    attr = OrderedDict()
-    if not attr_str:
-        return attr
+class Attr(OrderedDict):
 
-    for hit in attr_regex.finditer(attr_str):
-        value = hit.group(2)
-        if (value[0] == "\"" and value[-1] == "\"") or (value[0] == "\'" and value[-1] == "\'"):
-            attr[hit.group(1)] = value[1:-1]
-        else:
-            attr[hit.group(1)] = value
-    return attr
+    attr_regex = re.compile(r"([\w\:]+?)\s*=\s*(\"[^\"\'<>]+?\"|\'[^<>\']+?\')")
+    
+    def __init__(self, attr_str):
+        for hit in self.attr_regex.finditer(attr_str):
+            value = hit.group(2)
+            if (value[0] == "\"" and value[-1] == "\"") or (value[0] == "\'" and value[-1] == "\'"):
+                self[hit.group(1)] = value[1:-1]
+            else:
+                self[hit.group(1)] = value
+
 
 class Tag:
 
@@ -35,34 +34,35 @@ class Tag:
             re.DOTALL
         )
 
-    def finditer(self, fileobj, batch_len=5000):
+    def finditer_from_file(self, fileobj, chunk_size=300000):
         xml = ""
 
-        i = 0
-        for line in fileobj:
-            i += 1
-            xml += line
-            if i % batch_len:
-                continue
+        chunk = fileobj.read(chunk_size)
+        while chunk:
+            xml += chunk
 
-            matches = [m for m in self.finditer_str(xml)]
-            if matches:
-                xml = xml[matches[-1].end():]  
-                for m in matches:
-                    yield m
+            end_index = 0
+            for m in self.finditer(xml):
+                end_index = m.end()
+                yield m
+            
+            if end_index:
+                xml = xml[end_index:]
+
+            chunk = fileobj.read(chunk_size)
         
-        for m in self.finditer_str(xml):
+        for m in self.finditer(xml):
             yield m
 
-
-    def finditer_str(self, string):
+    def finditer(self, string):
         for g in self.tag_regex.finditer(string):
             yield g
     
-    def search(self, iterable):
-        return self.finditer(iterable)
+    def search_from_file(self, fileobj, chunk_size=300000):
+        for m in self.finditer_from_file(fileobj, chunk_size=300000):
+            return m
 
-    def search_str(self, string):
+    def search(self, string):
         return self.tag_regex.search(string)
 
 
