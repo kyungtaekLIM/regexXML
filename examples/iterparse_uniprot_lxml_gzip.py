ce786c0ef7e6ca138afca2c8094cab0b72ed75d7
kyungtaekLIM <shallowest.sea@gmail.com>
2017-07-10 01:10:00 +0900

    add lxml examples

--- examples/iterparse_uniprot_lxml_gzip.py
+++ examples/iterparse_uniprot_lxml_gzip.py
@@ -1,13 +1,14 @@
 import os
+import gzip
 from lxml import etree
 
-filename = "uniprot_sprot.xml"
+filename = "uniprot_sprot.xml.gz"
 
 if not os.path.isfile(filename):
     err_tmpl = "%s was not found. Download ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.xml.gz"
     raise IOError(err_tmpl % filename)
 
-with open(filename) as f:
+with gzip.open(filename) as f:
     i = 0
     for event, entry in etree.iterparse(f, events=("end",), tag="entry"):
         for gene in entry.findall(".//gene"):
